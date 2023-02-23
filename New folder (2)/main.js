c9b48e4f7c582d43af59e4618f8650160bd83d25
BaxisliNuray <bakishlinuray@gmail.com>
2023-02-23 00:47:38 -0800

    created file

---
+++ New folder (2)/main.js
@@ -0,0 +1,44 @@
+let x = 4;
+let a = 6;
+let m = "Nuray"
+
+
+let k = x + a + m 
+console.log(k);
+
+//Arithmetic
+console.log (x+a);
+console.log (x-a);
+console.log(x*a);
+console.log(x**a);
+console.log(a/x);
+console.log(a%x);
+console.log(++x);
+console.log(--a);
+
+//Assigment
+console.log(x+=a);
+console.log(a-=x);
+console.log(a*=x);
+console.log(a/x);
+console.log(x%a);
+console.log(x**=a);
+
+
+
+//Comparasion
+console.log(x==a);
+console.log(x===a);
+console.log(x!=m);
+console.log(x!=a);
+console.log(x>m);
+console.log(x<a);
+console.log(x>=a);
+console.log(a<=a);
+
+
+//logical
+console.log(x<a && a>m);
+console.log(a>=x || m<=a);
+
+
